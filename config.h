2301efae196185b941ec7730da6c425eab47e6ae
ZhouXing <zhouxing.xgg@qq.com>
2021-12-04 12:33:31 +0100

    hide cursor when typing

--- config.h
+++ config.h
@@ -147,7 +147,7 @@ unsigned int defaultunderline = 7;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 6;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
